cc36ad53a751c50da86f36f6b23c1cd22026e550
Tunga G <96535304+Tunga16@users.noreply.github.com>
2022-02-04 04:15:43 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
 
+void input (int *a , int*b)
+{
+  printf("Enter the value of a");
+  scanf("%d , a");
+  printf("Enter the value of b");
+  scanf("%d , b");
+}
+void add (int a, int b, int * sum)
+{
+  *sum=a+b;
+}
+void output (inta, int b, int sum)
+{
+  printf ("The sum of %d and %d is %d" ,a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a, &b);
+  add(a,b,&sum);
+  output(a.b.sum);
+  return 0;
 }
