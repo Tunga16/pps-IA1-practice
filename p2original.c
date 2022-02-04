ae52dbf508ec11e6d3f6dd53bdb21e5075bc16f3
Tunga G <96535304+Tunga16@users.noreply.github.com>
2022-02-04 04:53:06 +0000

    p2original.c

--- p2original.c
+++ p2original.c
@@ -1,1 +1,29 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int *a,int *b,int *c)
+{
+  printf("enter a no.");
+  scanf("%d%d%d",a,b,c);
+}
+void cmp(int a,int b,int c,int *l)
+{ 
+  if(a>b&&a>c)
+  *l=a;
+  if(b>a&&b>c)
+  *l=b;
+  else
+  if(c>a&&c>b)
+  *l=c;
+}
+void output(int largest)
+{
+  printf("the largest number in the given input is;%d",largest);
+}
+int main()
+{
+  int a,b,c,largest;
+  input(&a,&b,&c);
+  cmp(a,b,c,&largest);
+  output(largest);
+  return 0;
+}
+
