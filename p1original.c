fca3f430b8721458b2a2e074c6f030c2d1e53333
Tunga G <96535304+Tunga16@users.noreply.github.com>
2022-02-04 04:24:11 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -3,23 +3,23 @@
 void input (int *a , int*b)
 {
   printf("Enter the value of a");
-  scanf("%d , a");
+  scanf("%d", a);
   printf("Enter the value of b");
-  scanf("%d , b");
+  scanf("%d", b);
 }
 void add (int a, int b, int * sum)
 {
   *sum=a+b;
 }
-void output (inta, int b, int sum)
+void output (int a, int b, int sum)
 {
-  printf ("The sum of %d and %d is %d" ,a,b,sum);
+  printf ("The sum of %d and %d is %d",a,b,sum);
 }
 int main()
 {
   int a,b,sum;
   input(&a, &b);
   add(a,b,&sum);
-  output(a.b.sum);
+  output(a,b,sum);
   return 0;
 }
